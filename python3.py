f823698d9971230f29755dfaa3da2f8c5676bfe1
Shylaja140 <mukilanmukil404@gmail.com>
2025-07-31 14:50:54 +0530

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,4 @@
+if num%2==0:
+    print ("EVEN")
+    else:
+        print("odd")
---
+++ pyton.py
@@ -0,0 +1,6 @@
+print("Hello world")
+num=int(input ("Enter a number"))
+a=num+10
+b=num*2
+print(a)
+print(bm////lll.,.l,
